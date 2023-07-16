176c12a21c3f7489db1b6204c06e66941574697d
ShubhaM57 <47097387+ShubhaM57@users.noreply.github.com>
2023-07-16 15:35:18 +0100

    first commit

---
+++ CSharpFeature/Program.cs
@@ -0,0 +1,13 @@
+﻿using CSharpFeature.CSharp11Features;
+
+// Raw String literals
+//StringFeatures.StringLiterals();
+
+//Raw String literals with String interpolation
+//StringFeatures.StringLiteralsWithInterpolation();
+
+// String Expressions
+//StringFeatures.StringExpressions();
+
+// Method group Conversions
+MethodGroupConvertions.Assertions();
